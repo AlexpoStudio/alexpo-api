b5441dfe6ec799e265e857e2b7dacb5203b2a47d
apotapov@telematika.com <alexpostudiogames@gmail.com>
2023-11-27 16:59:23 +0500

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyNzU1Mg/spectral.js?branch_slug=master&token=33f3e06f-85a4-4852-86b1-c29f8a3097fa";
+export default { extends: ruleset };
